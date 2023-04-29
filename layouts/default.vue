2dfc4a7cde190959520596b58dceef4692f0366c
Nagi Nagisa <nagi_nagisa111@yahoo.co.jp>
2023-04-29 23:18:22 +0900

    sp menu対応

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,121 @@
 <template>
     <v-app>
         <v-app-bar
-            density="prominent"
-            class="bg-transparent"
-            flat
+                density="prominent"
+                class="bg-transparent"
+                flat
         >
             <v-app-bar-title>
                 <v-img
-                    height="128"
-                    width="250"
-                    src="./imgs/logo/logo.png"
-                    >
+                        height="128"
+                        width="250"
+                        src="./imgs/logo/logo.png"
+                >
                 </v-img>
 
             </v-app-bar-title>
+            <div
+                    v-if="$vuetify.display.mdAndUp"
+            >
+
+                <v-btn
+                        href="#profile"
+                        size="large">
+                    Profile
+                </v-btn>
+                <v-btn
+                        href="#activities"
+                        size="large">
+                    Activities
+                </v-btn>
+                <v-btn
+                        href="#live"
+                        size="large">
+                    Live
+                </v-btn>
+                <v-btn
+                        href="#gallery"
+                        size="large">
+                    Gallery
+                </v-btn>
+
+            </div>
             <v-btn
-                href="#profile"
-                size="large">
-                Profile
-            </v-btn>
-            <v-btn
-                href="#activities"
-                size="large">
-                Activities
-            </v-btn>
-            <v-btn
-                href="#live"
-                size="large">
-                Live
-            </v-btn>
-            <v-btn
-                href="#gallery"
-                size="large">
-                Gallery
+                    @click.stop="navActive = !navActive"
+                    size="large"
+            >
+                <div
+                        class="hamburger-menu"
+                        v-bind:class="{active: navActive}"
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </v-btn>
 
         </v-app-bar>
+
+        <v-navigation-drawer
+                temporary
+                v-model="navActive"
+                location="left"
+                :width="$vuetify.display.width"
+                class=""
+                color="blue-lighten-5"
+                elevation="0"
+                disable-resize-watcher
+        >
+            <v-container class="position-relative">
+                <v-img
+                        style="z-index: 1;"
+                        height="125"
+                        src="./imgs/logo/logo.png"
+                ></v-img>
+                <v-list
+                        style="z-index: 1;"
+                >
+                    <v-list-item>
+                        <v-btn
+                                variant="text"
+                                href="#profile"
+                                size="large">
+                            Profile
+                        </v-btn>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-btn
+                                variant="text"
+                                href="#activities"
+                                size="large">
+                            Activities
+                        </v-btn>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-btn
+                                variant="text"
+                                href="#live"
+                                size="large">
+                            Live
+                        </v-btn>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-btn
+                                variant="text"
+                                href="#gallery"
+                                size="large">
+                            Gallery
+                        </v-btn>
+                    </v-list-item>
+                </v-list>
+                <v-img class="position-fixed"
+                       style="bottom: 0; left:0; max-height: 800px; z-index: 0;"
+                       :width="$vuetify.display.width"
+                       src="./imgs/profile/profile.png"
+                >
+                </v-img>
+            </v-container>
+        </v-navigation-drawer>
 
         <main>
             <slot></slot>
@@ -85,8 +165,47 @@
 
 <script setup lang="ts">
 import {mdiTwitter, mdiYoutube} from "@mdi/js";
+
+const navActive = ref(false)
+const drawer = ref(false)
 </script>
 
 <style scoped lang="scss">
+
+.hamburger-menu {
+  position: relative;
+}
+
+.hamburger-menu span {
+  position: absolute;
+  top: -4px;
+  left: -15px;
+  background-color: black;
+  transition-property: opacity, transform;
+  transition-duration: .5s;
+  height: 3px;
+  width: 30px;
+}
+
+.hamburger-menu span:nth-child(1) {
+  top: -12px;
+}
+
+.hamburger-menu.active span:nth-child(1) {
+  transform: translateY(8px) rotate(405deg);
+}
+
+.hamburger-menu.active span:nth-child(2) {
+  opacity: 0;
+}
+
+.hamburger-menu span:nth-child(3) {
+  top: 4px;
+}
+
+.hamburger-menu.active span:nth-child(3) {
+  transform: translateY(-8px) rotate(-405deg);
+}
+
 
 </style>
