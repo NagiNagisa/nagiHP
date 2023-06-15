a55bf72081a0b5b66e2ebd15324f193f4d24305d
Nagi Nagisa <nagi_nagisa111@yahoo.co.jp>
2023-06-15 11:46:51 +0900

    add ogp, meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,23 @@ export default defineNuxtConfig({
     app:{
         // baseURL: '/nagiHP/',
         // cdnURL: '/nagiHP/'
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: '凪なぎさ VTuberの公式ホームページ',
+            htmlAttrs: {
+                lang: 'ja',
+            },
+            meta: [
+                { property: 'og:url', content: 'https://site.naginagisa.com/' },
+                { property: 'og:title', content: '凪なぎさ VTuberの公式ホームページ' },
+                { property: 'og:site:name', content: '凪なぎさ VTuberの公式ホームページ' },
+                { property: 'og:image', content: 'https://site.naginagisa.com/imgs/og_image.png' },
+                { property: 'og:type', content: 'website' },
+                { name: 'description', content: 'VTuber凪なぎさの公式ホームページです。ピアノ、プログラミング、将棋など幅広い分野で活躍しています。' },
+                { property: 'og:description', content: 'VTuber凪なぎさの公式ホームページです。ピアノ、プログラミング、将棋など幅広い分野で活躍しています。' },
+            ],
+        }
     },
     typescript: {
         shim: false,
